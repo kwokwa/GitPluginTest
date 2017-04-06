66f5597a8954539bd6d3c88a009a8c70714bee55
Wai K <my@email.com>
2017-04-06 11:54:46 -0400

    removed bug fix

--- src/GitPluginTest.java
+++ src/GitPluginTest.java
@@ -1,6 +1,6 @@
 
 public class GitPluginTest {
 	 public static void main(String[] args) {
-		 System.out.println("Hello World there...bug#1234");
+		 System.out.println("Hello World there...");
 	 }
 }
