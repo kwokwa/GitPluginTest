4a9933307184630b93b12634de62575b3b0e64c4
Wai K <my@email.com>
2017-04-06 10:52:54 -0400

    bug #1234 fixed

--- src/GitPluginTest.java
+++ src/GitPluginTest.java
@@ -1,6 +1,6 @@
 
 public class GitPluginTest {
 	 public static void main(String[] args) {
-		 System.out.println("Hello World there...");
+		 System.out.println("Hello World there...bug#1234");
 	 }
 }
